084e2052e2451339dd8a890e879c1cf1fbdaced5
yvanoide <132450178+yvanoide@users.noreply.github.com>
2024-11-04 13:27:54 +0100

    Delete web_traducteur/src directory

--- web_traducteur/src/app.py
+++
@@ -1,5 +0,0 @@
-# main.py
-from vue.traducteur_app import TraducteurApp
-
-if __name__ == "__main__":
-    TraducteurApp()
--- web_traducteur/src/config/parametres.py
+++
@@ -1,4 +0,0 @@
-URL_TRADUCTEUR = "http://127.0.0.1:8088/traducteur"
-URL_VERSIONS = "http://127.0.0.1:8088/versions"
-URL_LOGIN = "http://127.0.0.1:8088/login"
-URL_TRADUCTIONS = "http://127.0.0.1:8088/traductions/"
